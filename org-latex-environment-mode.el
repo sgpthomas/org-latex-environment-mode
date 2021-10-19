bfaaef09a183130cf8cfd6f6a34368b0bc6d03d6
Sammy Thomas <sgpthomas@gmail.com>
2021-10-18 22:43:39 -0500

    minimal code

---
+++ org-latex-environment-mode.el
@@ -0,0 +1,6 @@
+(provide 'org-latex-environment-mode)
+
+(define-minor-mode org-latex-environment-mode
+  "Adds a font-lock face for Latex environment block. Meant to be used inside of Org files."
+
+  )
